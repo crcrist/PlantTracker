3cc5332d7cf79cd6b6a8e6261157571738c2c1e0
Connor Crist <connorcrist27@gmail.com>
2024-09-06 06:09:19 -0500

    added functionality to add stocks to portfolio, and to remove by clicking x

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,37 @@ import CardList from "./Components/CardList/CardList"
 import Search from "./Components/Search/Search";
 import { searchCompanies } from './api';
 import { CompanySearch } from './company';
+import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log(e);
+    };
+
+    const onPortfolioCreate = (e: any) => {
+      e.preventDefault();
+      const exists = portfolioValues.find((value) => value === e.target[0].value)
+      if (exists) return;
+      const updatedPortfolio = [...portfolioValues, e.target[0].value];
+      setPortfolioValues(updatedPortfolio);
+    };
+
+    const onPortfolioDelete = (e: any) => {
+      e.preventDefault();
+      const removed = portfolioValues.filter((value) => {
+        return value !== e.target[0].value;
+      });
+      setPortfolioValues(removed);
     }
-    const onClick = async (e: SyntheticEvent) => {
+
+    const onSearchSubmit = async (e: SyntheticEvent) => {
+      e.preventDefault();
         const result = await searchCompanies(search);
         if(typeof result === "string") {
           setServerError(result);
@@ -25,8 +45,9 @@ function App() {
     };
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
-      <CardList searchResults={searchResult}/>
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
+      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete}/>
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
       {serverError && <div>Unable to connect to API</div>}
     </div>
   );
