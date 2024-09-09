a3137b839bd32d11160f9684f3f1418d283c5eb5
Connor Crist <connorcrist27@gmail.com>
2024-09-09 07:37:54 -0500

    stuck troubleshooting when implementing router, when opening browser blank page pops up

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { SearchCheck } from 'lucide-react';
-import { searchCompanies } from './api';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-console.log(searchCompanies("tsla"));
 root.render(
   <React.StrictMode>
+    < RouterProvider router={router} />
     <App />
   </React.StrictMode>
 );
