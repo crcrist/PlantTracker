adc09d211acc86172df24882bc2228c506b95e73
Connor Crist <connorcrist27@gmail.com>
2024-09-21 08:03:47 -0500

    successful call to grab company name being made from companypage

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
-import Axios, {AxiosError} from "axios";
-import {  CompanySearch} from "./company";
+import axios from "axios";
+import { CompanyProfile, CompanySearch } from "./company";
 
 export interface SearchResponse {
   data: CompanySearch[];
@@ -7,17 +7,28 @@ export interface SearchResponse {
 
 export const searchCompanies = async (query: string) => {
     try {
-        const data = await Axios.get<SearchResponse>(
+        const data = await axios.get<SearchResponse>(
             `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
         );
         return data;
     } catch(error) {
-        if(Axios.isAxiosError(error)) {
+        if (axios.isAxiosError(error)) {
             console.log("error message: ", error.message);
             return error.message;
         } else {
             console.log("unexpected error: ", error);
             return "An unexpected error has occured.";
         }
+    }
+};
+
+export const getCompanyProfile = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyProfile[]>(
+        `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+        );
+        return data;
+    } catch (error: any) {
+        console.log("error message from API: ", error.message);
     }
 };
